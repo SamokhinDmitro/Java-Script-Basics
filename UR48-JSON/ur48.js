70bcded51a99e660b1f1e895fc9a27f8c2b74096
Samokhin Dmitro <samokhin.dmitriy.dev@gmail.com>
2020-02-02 13:11:10 +0200

    Add work with JSON

---
+++ UR48-JSON/ur48.js
@@ -0,0 +1,26 @@
+/*
+1. Дана JSON строка '["Коля", "Вася", "Петя"]'. Преобразуйте ее в массив JavaScript и выведите на экран элемент "Петя".
+ */
+
+/*
+let str = '["Коля", "Вася", "Петя"]';
+alert(str);
+let p = document.createElement('p');
+p.innerHTML = JSON.parse(str)
+
+document.body.appendChild(p);
+*/
+
+/*
+2. Дан объект {a: 'aaa', b: 'bbb'}. Преобразуйте его в JSON
+ */
+/*
+let obj = {a: 'aaa', b: 'bbb'};
+
+let str = JSON.stringify(obj);
+alert(obj);
+alert(str);
+alert(typeof(str));
+*/
+
+
